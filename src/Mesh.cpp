2cf8c8f8db0254b901ea53a290c501ab5fa020f7
Brandon Boettcher <brandonboettcher19@gmail.com>
2023-02-10 05:29:24 -0600

    Add a faces array to mesh

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,6 +1,8 @@
 #include "Mesh.h"
 #include "Shader.h"
+#include "math/Face.h"
 #include <glad/glad.h>
+#include <vector>
 
 Mesh::Mesh() : m_vertexCount{ 0 } {
     glGenVertexArrays(1, &m_vertexArrayID);
@@ -16,6 +18,7 @@ void Mesh::setVertexData(unsigned int size, const void* data) {
     if (size == 0) {
         return;
     }
+
     // bind both buffers (vertex array first)
     glBindVertexArray(m_vertexArrayID);
     glBindBuffer(GL_ARRAY_BUFFER, m_vertexBufferID);
@@ -29,6 +32,35 @@ void Mesh::setVertexData(unsigned int size, const void* data) {
 
     // store the number of vertices
     m_vertexCount = size / sizeof(unsigned int);
+
+    // set the face data (used for collisions)
+    getFaces(reinterpret_cast<const unsigned int*>(data));
+}
+
+void Mesh::getFaces(const unsigned int* data) {
+    m_faces.reserve(m_vertexCount / 6);
+    for (unsigned int i = 0; i < m_vertexCount; i += 6) {
+        // each face has 6 vertices. However, the xyz coordinates of the 3rd
+        // and 4th vertex are the same, as well as the 1st and 6th (seen in
+        // Blockinfo.h). So take the 1st, 2nd, 3rd, and 5th vertex.
+        unsigned int Ax = (data[i] >> 23) & 0x1F;
+        unsigned int Ay = (data[i] >> 15) & 0xFF;
+        unsigned int Az = (data[i] >> 10) & 0x1F;
+        unsigned int Bx = (data[i + 1] >> 23) & 0x1F;
+        unsigned int By = (data[i + 1] >> 15) & 0xFF;
+        unsigned int Bz = (data[i + 1] >> 10) & 0x1F;
+        unsigned int Cx = (data[i + 2] >> 23) & 0x1F;
+        unsigned int Cy = (data[i + 2] >> 15) & 0xFF;
+        unsigned int Cz = (data[i + 2] >> 10) & 0x1F;
+        unsigned int Dx = (data[i + 4] >> 23) & 0x1F;
+        unsigned int Dy = (data[i + 4] >> 15) & 0xFF;
+        unsigned int Dz = (data[i + 4] >> 10) & 0x1F;
+        sglm::vec3 A = { (float) Ax, (float) Ay, (float) Az };
+        sglm::vec3 B = { (float) Bx, (float) By, (float) Bz };
+        sglm::vec3 C = { (float) Cx, (float) Cy, (float) Cz };
+        sglm::vec3 D = { (float) Dx, (float) Dy, (float) Dz };
+        m_faces.emplace_back(Face(A, B, C, D));
+    }
 }
 
 unsigned int Mesh::getVertexCount() const {
--- src/Mesh.h
+++ src/Mesh.h
@@ -2,11 +2,14 @@
 #define MESH_H_INCLUDED
 
 #include "Shader.h"
+#include "math/Face.h"
+#include <vector>
 
 class Mesh {
     unsigned int m_vertexArrayID;
     unsigned int m_vertexBufferID;
     unsigned int m_vertexCount;
+    std::vector<Face> m_faces;
 
 public:
     Mesh();
@@ -15,6 +18,9 @@ public:
     void setVertexData(unsigned int size, const void* data);
     unsigned int getVertexCount() const;
     void render(const Shader* shader) const;
+
+private:
+    void getFaces(const unsigned int* data);
 };
 
 #endif
